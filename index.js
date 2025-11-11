af774d5d9f3c6092753f9069214f5eb897e24d9a
Brendon W <bwherry@midland.k12.ia.us>
2025-11-11 18:04:46 +0000

    1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function guessNumber {
+    if(25 != '25') {
+        return null;
+    }
+
+ if(25 != 25) {
+        return null;
+    } else {
+        return "not it"
+    }
+};
+
+function strictGuessNumber (guess = 25) {
+    if(guess !== 25) {
+        return null;
+    }
+
+    if(guess !== '25') {
+        return "not it";
+    }
+}
+
+
+
+
 
